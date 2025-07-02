8ab5e596f2ad79bef39cfa8d624784cce75d9c27
pragin <pragin.martin@hotmail.com>
2025-07-02 23:35:20 +0100

    Get constituents with 7B market cap

--- tests/ftse100.spec.ts
+++ tests/ftse100.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, Page } from '@playwright/test';
 import { getConstituents } from '../utils/constituentsHelper';
 
-test.describe('FTSE 100 latest top 10 constituents', () => {
+test.describe('FTSE 100', () => {
   let FTSE100Page: Page;
   test.beforeEach(async ({ page, context }) => {
     await page.goto('/', { waitUntil: 'domcontentloaded' });
@@ -19,7 +19,7 @@ test.describe('FTSE 100 latest top 10 constituents', () => {
 
     await FTSE100Page.waitForLoadState('domcontentloaded', { timeout: 10000 });
   });
-  test('with the highest percentage change', async () => {
+  test('latest top 10 constituents with the highest percentage change', async () => {
     await expect(FTSE100Page).toHaveURL(
       /\/indices\/ftse-100\/constituents\/table/,
       { ignoreCase: true, timeout: 20000 }
@@ -30,7 +30,7 @@ test.describe('FTSE 100 latest top 10 constituents', () => {
       .locator('div')
       .click();
 
-    const rows: Record<string, string>[] = await getConstituents(FTSE100Page);
+    const rows: Record<string, string>[] = await getConstituents(FTSE100Page, 11);
 
     console.log(
       'FTSE 100’s latest top 10 constituents with the highest percentage change'
@@ -38,7 +38,7 @@ test.describe('FTSE 100 latest top 10 constituents', () => {
     console.table(rows);
   });
 
-  test('with the lowest percentage change', async ({ page, context }) => {
+  test('latest top 10 constituents with the lowest percentage change', async () => {
     await expect(FTSE100Page).toHaveURL(
       /\/indices\/ftse-100\/constituents\/table/,
       { ignoreCase: true, timeout: 20000 }
@@ -50,11 +50,46 @@ test.describe('FTSE 100 latest top 10 constituents', () => {
       .locator('div')
       .click();
 
-    const rows: Record<string, string>[] = await getConstituents(FTSE100Page);
+    const rows: Record<string, string>[] = await getConstituents(FTSE100Page, 11);
 
     console.log(
       'FTSE 100’s latest top 10 constituents with the lowest percentage change'
     );
     console.table(rows);
+  });
+
+  test('with over 7 billion market cap', async () => {
+    await expect(FTSE100Page).toHaveURL(
+      /\/indices\/ftse-100\/constituents\/table/,
+      { ignoreCase: true, timeout: 20000 }
+    );
+    await FTSE100Page.getByText('Market cap (m)').click();
+    await FTSE100Page.getByRole('listitem')
+      .filter({ hasText: 'Highest – lowest' })
+      .locator('div')
+      .click();
+
+    const paginationLinks = await FTSE100Page.locator('.paginator a').all();
+
+    let rows: Record<string, string>[] = [];
+
+    for (const link of paginationLinks) {
+      const isActive = (await link.getAttribute('class'))?.includes('active');
+      if (isActive) {
+        
+        rows.push(...await getConstituents(FTSE100Page));
+        continue;
+      }
+      await link.click();
+      await FTSE100Page.waitForLoadState('domcontentloaded');
+      await expect(link).toHaveClass('page-number active');
+      rows.push(...await getConstituents(FTSE100Page));
+    }
+   
+    const mCapOverSevenBillion = rows.filter(data => parseFloat(data['Market cap (m)'].replace(/,/g, '')) > 7000.00)                                  
+    console.log(
+      'FTSE 100’s constituents with over 7 billion market cap'
+    );
+    console.table(mCapOverSevenBillion)
   });
 });
--- utils/constituentsHelper.ts
+++ utils/constituentsHelper.ts
@@ -1,24 +1,29 @@
-import { Page } from '@playwright/test'
-export const getConstituents = async (FTSE100Page: Page): Promise<Record<string, string>[]> => {
-    const tableRows = await FTSE100Page.locator('table tr').all();
-    const first10Rows = tableRows.slice(0, 11);
+import { Locator, Page } from '@playwright/test';
+export const getConstituents = async (
+  FTSE100Page: Page,
+  noOfRows?: number
+): Promise<Record<string, string>[]> => {
+  const tableRows = await FTSE100Page.locator('table tr').all();
+  const rowsToExtract =
+    typeof noOfRows === 'number'
+      ? tableRows.slice(0, noOfRows)
+      : tableRows.slice(0);
 
-    const data: string[][] = [];
+  const data: string[][] = [];
 
-    for (const row of first10Rows) {
-      const cells: string[] = await row.locator('th,td').allInnerTexts();
+  for (const row of rowsToExtract) {
+    const cells: string[] = await row.locator('th,td').allInnerTexts();
 
-      if (cells.length != 0) {
-        data.push(cells);
-      }
+    if (cells.length != 0) {
+      data.push(cells);
     }
+  }
 
-    const headings: string[] = data[0];
-    const rows: Record<string, string>[] = data
-      .slice(1)
-      .map((row: string[]) =>
-        Object.fromEntries(row.map((cell, i) => [headings[i], cell]))
-      );
-    return rows
-}
-
+  const headings: string[] = data[0];
+  const rows: Record<string, string>[] = data
+    .slice(1)
+    .map((row: string[]) =>
+      Object.fromEntries(row.map((cell, i) => [headings[i], cell]))
+    );
+  return rows;
+};
