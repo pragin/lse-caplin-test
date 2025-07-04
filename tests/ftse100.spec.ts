4fcb39fa83b9a5cc72b6884dbf3a4d84a90a8c1b
pragin <pragin.martin@hotmail.com>
2025-07-04 19:15:44 +0100

    Update settings

--- tests/ftse100.spec.ts
+++ tests/ftse100.spec.ts
@@ -21,20 +21,20 @@ test.describe('FTSE 100', () => {
   });
   test('latest top 10 constituents with the highest percentage change', async () => {
     //Verify the user is in constitents page
-    await expect(FTSE100Page).toHaveURL(
-      /\/indices\/ftse-100\/constituents\/table/,
-      { ignoreCase: true, timeout: 20000 }
-    );
+    await expect(FTSE100Page).toHaveURL(/\/indices\/ftse-100\/constituents\/table/, {
+      ignoreCase: true,
+      timeout: 20000,
+    });
 
     //Verify the user is in FTSE 100 index page
-    await expect(
-      FTSE100Page.getByRole('heading', { name: 'FTSE 100' })
-    ).toBeVisible({ timeout: 5000 });
+    await expect(FTSE100Page.getByRole('heading', { name: 'FTSE 100' })).toBeVisible({
+      timeout: 5000,
+    });
 
     // Verify the table has been selected
-    await expect(
-      FTSE100Page.getByRole('listitem').filter({ hasText: /Table/i })
-    ).toHaveClass(/active/);
+    await expect(FTSE100Page.getByRole('listitem').filter({ hasText: /Table/i })).toHaveClass(
+      /active/
+    );
 
     // Order by Highest to lowest
     await FTSE100Page.getByText('Change %').click();
@@ -52,29 +52,24 @@ test.describe('FTSE 100', () => {
     );
 
     // Use the helper function to extract and return the top 10 constituents with highest % change
-    const rows: Record<string, string>[] = await getConstituents(
-      FTSE100Page,
-      11
-    );
+    const rows: Record<string, string>[] = await getConstituents(FTSE100Page, 11);
 
-    console.log(
-      'FTSE 100’s latest top 10 constituents with the highest percentage change'
-    );
+    console.log('FTSE 100’s latest top 10 constituents with the highest percentage change');
     console.table(rows);
   });
 
   test('latest top 10 constituents with the lowest percentage change', async () => {
-    await expect(FTSE100Page).toHaveURL(
-      /\/indices\/ftse-100\/constituents\/table/,
-      { ignoreCase: true, timeout: 20000 }
-    );
+    await expect(FTSE100Page).toHaveURL(/\/indices\/ftse-100\/constituents\/table/, {
+      ignoreCase: true,
+      timeout: 20000,
+    });
 
-    await expect(
-      FTSE100Page.getByRole('heading', { name: 'FTSE 100' })
-    ).toBeVisible({ timeout: 5000 });
-    await expect(
-      FTSE100Page.getByRole('listitem').filter({ hasText: /Table/i })
-    ).toHaveClass(/active/);
+    await expect(FTSE100Page.getByRole('heading', { name: 'FTSE 100' })).toBeVisible({
+      timeout: 5000,
+    });
+    await expect(FTSE100Page.getByRole('listitem').filter({ hasText: /Table/i })).toHaveClass(
+      /active/
+    );
 
     await FTSE100Page.getByText('Change %').click();
     await FTSE100Page.getByRole('listitem')
@@ -89,22 +84,17 @@ test.describe('FTSE 100', () => {
         resp.status() == 200
     );
 
-    const rows: Record<string, string>[] = await getConstituents(
-      FTSE100Page,
-      11
-    );
+    const rows: Record<string, string>[] = await getConstituents(FTSE100Page, 11);
 
-    console.log(
-      'FTSE 100’s latest top 10 constituents with the lowest percentage change'
-    );
+    console.log('FTSE 100’s latest top 10 constituents with the lowest percentage change');
     console.table(rows);
   });
 
   test('constituents with over 7 billion market cap', async () => {
-    await expect(FTSE100Page).toHaveURL(
-      /\/indices\/ftse-100\/constituents\/table/,
-      { ignoreCase: true, timeout: 20000 }
-    );
+    await expect(FTSE100Page).toHaveURL(/\/indices\/ftse-100\/constituents\/table/, {
+      ignoreCase: true,
+      timeout: 20000,
+    });
     await FTSE100Page.getByText('Market cap (m)').click();
     await FTSE100Page.getByRole('listitem')
       .filter({ hasText: 'Highest – lowest' })
@@ -136,14 +126,14 @@ test.describe('FTSE 100', () => {
   });
 
   test('month over the past three years recorded the lowest average index value', async () => {
-    await expect(FTSE100Page).toHaveURL(
-      /\/indices\/ftse-100\/constituents\/table/,
-      { ignoreCase: true, timeout: 20000 }
-    );
+    await expect(FTSE100Page).toHaveURL(/\/indices\/ftse-100\/constituents\/table/, {
+      ignoreCase: true,
+      timeout: 20000,
+    });
 
     const fromDate: Date = new Date();
     fromDate.setFullYear(fromDate.getFullYear() - 3);
-    
+
     await FTSE100Page.getByRole('link', { name: 'Overview' }).click();
 
     await FTSE100Page.waitForLoadState('domcontentloaded', { timeout: 10000 });
@@ -180,10 +170,7 @@ test.describe('FTSE 100', () => {
 
     let responseBody: any = await response.json();
 
-    console.log(responseBody)
-
-    const fromDateFromResponse:Date = responseBody.request.fromDate
-    console.log(fromDateFromResponse)
+    const fromDateFromResponse: Date = responseBody.request.fromDate;
 
     type data = { [key: string]: any; CLOSE_PRC: string; _DATE_END: string };
     const result: data = responseBody.data.reduce((min: data, curr: data) =>
@@ -193,12 +180,12 @@ test.describe('FTSE 100', () => {
     const lowestAvgIndexValue: string = result['CLOSE_PRC'];
     const date: Date = new Date(result['_DATE_END']);
     const year = date.getFullYear();
-    const month = date.toLocaleString('default', { month: 'long' })
-   
-    const expectedDate = `${fromDate.getFullYear()}-${(fromDate.getMonth() + 1).toString().padStart(2,'0')}-${fromDate.getDate().toString().padStart(2, '0')}`
+    const month = date.toLocaleString('default', { month: 'long' });
+
+    const expectedDate = `${fromDate.getFullYear()}-${(fromDate.getMonth() + 1).toString().padStart(2, '0')}-${fromDate.getDate().toString().padStart(2, '0')}`;
 
     // Verify the response has from date
-    expect(fromDateFromResponse.toString()).toContain(expectedDate)
+    expect(fromDateFromResponse.toString()).toContain(expectedDate);
 
     console.log(
       `${month},${year} had the lowest avereage index value over the past three years with ${lowestAvgIndexValue}`
